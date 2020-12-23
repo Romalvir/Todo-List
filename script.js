a1197d662788371f24a5ea6faf11a335da0e5301
Romalvir Singh <romiie91@gmail.com>
2020-12-22 22:28:42 -0500

    DOM and CSS

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var addButton = document.getElementById("add-button");
 var clearButton = document.getElementById("clear-completed-button");
 var emptyButton = document.getElementById("empty-button");
 var saveButton = document.getElementById("save-button");
+
 //Grab on to the input box
 var toDoEntryBox = document.getElementById("todo-entry-box");
 //Grab on to the list element in the HTML
@@ -16,18 +17,35 @@ emptyButton.addEventListener("click", emptyList)
 saveButton.addEventListener("click", saveList)
 
 //Functions for the Event Listeners
+
 function addToDoItem(){
 	//alert ("The Add button was clicked");
+	var itemText = toDoEntryBox.value;
+	newToDoItem(itemText, false);
+	//Since a new to-do item is never complete, you can 
+	// pass false to the completed parameter of the newToDoItem function.
 }
 
 
 function clearCompletedToDoItems(){
 	//alert ("The clear button was clicked");
+	var completedItems = toDoList.getElementsByClassName("completed");
+
+	while (completedItems.length > 0) {
+        completedItems.item(0).remove();
+    }
+
 }
 
 
 function emptyList(){
 	//alert ("The empty button was clicked");
+	var toDoItems = toDoList.children;
+	
+    while (toDoItems.length > 0) {
+        toDoItems.item(0).remove();
+    }
+
 }
 
 
@@ -48,7 +66,28 @@ function newToDoItem (itemText, completed){
 	//Append to HTML
 	toDoItem.appendChild(toDoText);
 
-	// we add an if statement. if the 
+	//checks if the value for the completed variable that was passed to newToDoItem is true. 
+	//If it is, then it will add the class completed to the li element, which will change how it looks on the page.
+	if (completed) {
+    toDoItem.classList.add("completed");
+	}
 
+	toDoList.appendChild(toDoItem);
+	//puts toDoItem (the <li> element) inside of toDoList (the <ol> element).
 
+	toDoItem.addEventListener("dblclick", toggleToDoItemState);
 }
+
+function toggleToDoItemState() {
+    if (this.classList.contains("completed")) {
+        this.classList.remove("completed");
+    } else {
+        this.classList.add("completed");
+    }
+}
+
+
+
+
+
+
